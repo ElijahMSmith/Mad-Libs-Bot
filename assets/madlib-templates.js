37f87fb165d7fa2c70c4ebc0ff2e2c862d969752
Eli <mcditto99@gmail.com>
2022-03-24 13:47:39 -0400

    assets

---
+++ assets/madlib-templates.js
@@ -0,0 +1,21 @@
+const MadLib = {
+    text: "One day I was {0} at the local {1} when {2} {3} past me pushing a {4} in a {5}. The {4} was very {6} and casually {7}. {2} had a smile on his face and was singing “{8}” for everyone to hear. “{9}!” I shouted a bit too {10}. Everyone turned and {11} at me, so I put my head down and {12}. I went home to do the one thing I hate the most: {13}.",
+    requirements: [
+        "Verb: 'ing'",
+        "Noun",
+        "Person's Name",
+        "Verb: 'ed'",
+        "Noun",
+        "Noun",
+        "Adjective",
+        "Verb: 'ing'",
+        "Name of Song",
+        "Exclamation",
+        "Adjective",
+        "Verb: 'ed'",
+        "Verb: 'ed'",
+        "Activity",
+    ],
+};
+
+export default MadLib;
